3dce748e0bd493fa8483563e64bab0de89160517
Mike Codeur <cabanas.mickael@gmail.com>
2021-09-15 16:55:46 +0800

    App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
-//export * from './App.final'
+import {NetflixApp} from 'components/NetflixApp'
+import {createTheme, ThemeProvider} from '@material-ui/core/styles'
 
-export * from './App.exercise'
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#111',
+    },
+    secondary: {
+      main: '#000',
+    },
+  },
+})
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <NetflixApp />
+    </ThemeProvider>
+  )
+}
+
+export {App}
