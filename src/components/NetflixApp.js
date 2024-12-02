d66b986a7c7e4633666737698300e2be009a2897
Ons HAMAD <onshamadkl@gmail.com>
2024-12-02 19:07:22 +0100

    migration

--- src/components/NetflixApp.js
+++ src/components/NetflixApp.js
@@ -1,6 +1,6 @@
-//export * from './NetflixApp.final'
+// export * from './NetflixApp.final'
 
 export * from './NetflixApp.exercise'
 
 // 🚀 Effet sur la barre de menu (scrool)
-//export * from './NetflixApp.bonus-1'
+// export * from './NetflixApp.bonus-1'
